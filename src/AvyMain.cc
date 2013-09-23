b2fef8f556dfc6a23acec5441134bd2378dcc951
Arie Gurfinkel <arie@cmu.edu>
2013-09-23 18:18:15 -0400

    main avy loop

--- src/AvyMain.cc
+++ src/AvyMain.cc
@@ -1,6 +1,7 @@
 #include "AvyMain.h"
 #include "boost/lexical_cast.hpp"
 #include "avy/Util/Global.h"
+#include "SafetyVC.h"
 
 #include "base/main/main.h"
 #include "aig/ioa/ioa.h"
@@ -9,6 +10,8 @@ using namespace boost;
 using namespace std;
 using namespace abc;
 using namespace avy;
+
+
 
 namespace abc
 {
@@ -32,7 +35,7 @@ static Aig_Man_t *loadAig (std::string fname)
 
 namespace avy
 {
-  AvyMain::AvyMain (std::string fname) : m_fName (fname)
+  AvyMain::AvyMain (std::string fname) : m_fName (fname), m_Vc (0), m_Solver(2, 2)
   {
     VERBOSE (2, vout () << "Starting ABC\n");
     Abc_Start ();
@@ -62,8 +65,55 @@ namespace avy
 
   int AvyMain::run ()
   {
+    SafetyVC vc (&*m_Aig);
+    m_Vc = &vc;
+
+    unsigned nMaxFrames = 100;
+    for (unsigned nFrame = 0; nFrame < 100; ++nFrame)
+      {
+        tribool res = doBmc (nFrame);
+        if (res)
+          {
+            VERBOSE (0, vout () << "SAT from BMC at frame: " << nFrame << "\n";);
+            return 1;
+          }
+        else if (!res)
+          {
+            VERBOSE(0, vout () << "UNSAT from BMC at frame: " << nFrame << "\n";);
+            
+          }
+        else 
+          {
+            VERBOSE (0, vout () << "UNKNOWN from BMC at frame: " 
+                     << nFrame << "\n";);
+            return 2;
+          }
+      }
+    
+    
     return 0;
   }
+
+  tribool AvyMain::doBmc (unsigned nFrame)
+  {
+    m_Solver.reset (nFrame + 2, m_Vc->varSize (0, nFrame, true));
+    
+    unsigned nOffset = 0;
+    
+    for (unsigned i = 0; i <= nFrame; ++i)
+      {
+        nOffset = m_Vc->addTrCnf (m_Solver, i, nOffset);
+        m_Solver.markPartition (i);
+        m_Solver.dumpCnf ("frame" + lexical_cast<string>(nFrame) + ".cnf");
+      }
+    nOffset = m_Vc->addBadCnf (m_Solver, nOffset);
+    m_Solver.markPartition (nFrame + 1);
+    m_Solver.dumpCnf ("frame" + lexical_cast<string>(nFrame+1) + ".cnf");
+
+    LitVector assumps;
+    return m_Solver.solve (assumps);
+  }
+  
   
 
 }
--- src/AvyMain.h
+++ src/AvyMain.h
@@ -2,7 +2,10 @@
 #define _AVYMAIN_H_
 
 #include "AigUtils.h"
+#include "SafetyVC.h"
+#include "ItpSatSolver.h"
 #include <string>
+#include "boost/logic/tribool.hpp"
 
 namespace avy
 {
@@ -11,16 +14,26 @@ namespace avy
     std::string m_fName;
     
     AigManPtr m_Aig;
+
+    /** reference to the current VC */
+    SafetyVC *m_Vc;
+    
+    /** refernece to the current Sat solver */
+    ItpSatSolver m_Solver;
     
   public:
     AvyMain(std::string fname);
     
     virtual ~AvyMain()
     {
+      
     }
     
 
     int run ();
+
+    boost::tribool doBmc (unsigned nFrame);
+    
   
   };
 }
