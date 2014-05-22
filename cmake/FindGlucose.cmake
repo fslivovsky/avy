01fa43a0ea628639884dd3f6e3c1a47ae182e8ae
Arie Gurfinkel <arie@cmu.edu>
2014-05-22 15:36:35 -0400

    cmake: use local versions of dependencies if available

--- cmake/FindGlucose.cmake
+++ cmake/FindGlucose.cmake
@@ -2,8 +2,13 @@
 
 set(GLUCOSE_ROOT "" CACHE PATH "Root of Glucose installation.")
 
-find_path(GLUCOSE_INCLUDE_DIR NAMES glucose/core/Solver.h PATHS ${GLUCOSE_ROOT}/include)
-find_library(GLUCOSE_LIBRARY  NAMES glucose  PATHS ${GLUCOSE_ROOT}/lib)
+if (DEFINED Glucose_SOURCE_DIR)
+  include ("${Glucose_SOURCE_DIR}/cmake/PackageOptions.cmake")
+else()
+  find_path(GLUCOSE_INCLUDE_DIR NAMES glucose/core/Solver.h 
+    PATHS ${GLUCOSE_ROOT}/include)
+  find_library(GLUCOSE_LIBRARY  NAMES glucose  PATHS ${GLUCOSE_ROOT}/lib)
+endif()
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Glucose
