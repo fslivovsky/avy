767606149e22bfb58a019462b78e0d9c390e3891
Yakir Vizel <yakir.vizel@gmail.com>
2015-09-06 09:26:25 -0400

    Adding CEX support to AvyBmc

--- src/AvyBmc.cc
+++ src/AvyBmc.cc
@@ -1,10 +1,12 @@
 #include <vector>
 #include <string>
+#include <fstream>
 
 #include "boost/logic/tribool.hpp"
 #include "boost/logic/tribool_io.hpp"
 #include "boost/noncopyable.hpp"
 #include "boost/program_options.hpp"
+#include "boost/scoped_ptr.hpp"
 namespace po = boost::program_options;
 
 #include "AigUtils.h"
@@ -28,6 +30,7 @@ namespace po = boost::program_options;
 
 
 using namespace boost;
+using namespace std;
 using namespace avy;
 using namespace avy::abc;
 
@@ -73,6 +76,9 @@ namespace avy
     
     template <typename Sat> 
     boost::tribool bmc (SafetyAigVC& vc, Sat &solver, Unroller<Sat>& unroller, unsigned uDepth);
+
+    template <typename Sat>
+    void printCex(Sat& s, Unroller<Sat>& unroller);
     
     void setCnfFile (std::string &cnf) { m_CnfFile = cnf; }
     
@@ -104,7 +110,10 @@ namespace avy
         for (unsigned f = 0; f <= uDepth; f++)
         {
             res = bmc (vc, sat, unroller, f);
-            if (res) break;
+            if (res) {
+                printCex(sat, unroller);
+                break;
+            }
         }
       }
     else
@@ -114,7 +123,10 @@ namespace avy
         for (unsigned f = uDepth; f <= uDepth; f++)
 		{
 		    res = bmc (vc, sat, unroller, f);
-		    if (res) break;
+		    if (res) {
+		        printCex(sat, unroller);
+		        break;
+		    }
 		}
       }
     
@@ -160,6 +172,79 @@ namespace avy
     vout().flush();
     return res;
   } 
+
+  template<typename Sat>
+    void AvyBmc::printCex(Sat& s, Unroller<Sat>& unroller)
+    {
+      printf("Here\n");
+      // -- skip cex if no output file is given
+      if (gParams.cexFileName.empty ()) return;
+      printf("Here\n");
+      AVY_ASSERT (!gParams.stutter);
+      AVY_ASSERT (gParams.tr0);
+
+      VERBOSE(2, vout () << "Generating CEX: " << gParams.cexFileName << "\n";);
+      boost::scoped_ptr<std::ofstream> pFout;
+      std::ostream *pOut;
+
+      if (gParams.cexFileName == "-")
+        pOut = &outs ();
+      else
+      {
+        pFout.reset (new std::ofstream (gParams.cexFileName.c_str (),
+                                        ofstream::out));
+        pOut = pFout.get ();
+      }
+
+      std::ostream &out = *pOut;
+      out << "1\n" << "b0\n";
+      int nRegs = Aig_ManRegNum(&*m_Aig);
+      // HACK: stick_error adds an extra latch
+      if (gParams.stick_error) nRegs--;
+
+      for (int i=0; i < nRegs; i++) out << "0";
+      out << "\n";
+
+      for (int i=0; i < unroller.frames (); i++)
+      {
+        abc::Vec_Int_t* PIs = unroller.getPrimaryInputs (i);
+
+        int j, input;
+
+        // -- in the first frame, first PI is the reset signal
+        if (i == 0)
+        {
+          input = Vec_IntEntry (PIs, 0);
+          // reset PI is on, current frame does not count
+          if (s.getVarVal(input)) continue;
+        }
+
+        Vec_IntForEachEntry(PIs, input, j)
+        {
+          // -- skipping the first input of the first and last
+          // -- frames. It is used for reset and is not part of the
+          // -- original circuit.
+          if (j == 0 /*&& (i == 0 || i + 1 == unroller.frames ())*/) continue;
+          out << (s.getVarVal(input) ? "1" : "0");
+        }
+        out <<  "\n";
+        if (gParams.stick_error && i + 1 < unroller.frames ())
+        {
+          abc::Vec_Int_t *vOuts = unroller.getOutputs (i);
+          int output = Vec_IntEntry (vOuts, Vec_IntSize (vOuts) - 1);
+          if (s.getVarVal (output))
+          {
+            VERBOSE(2, vout () << "Early CEX termination is untested!!!\n");
+            // -- reached an early end of the counter-example
+            break;
+          }
+        }
+      }
+
+      out << ".\n";
+      out << std::flush;
+
+    }
 }
 
 static std::string parseCmdLine (int argc, char**argv)
@@ -207,6 +292,9 @@ static std::string parseCmdLine (int argc, char**argv)
     ("glucose-inc-mode", 
      po::value<bool> (&gParams.glucose_inc_mode)->default_value(false)->implicit_value(true),
      "Enable Glucose incremental mode")
+    ("cex",
+     po::value<std::string> (&gParams.cexFileName)->default_value (""),
+     "Location to output counter-example")
 	("opt-bmc",
 	 po::value<bool> (&gParams.opt_bmc)->default_value(true)->implicit_value(true),
 	 "Enable optimized BMC");
